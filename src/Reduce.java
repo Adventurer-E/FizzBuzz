567b3d902ef474c3fd20abb42cd145027e14215f
Eric Lu <erickudou@163.com>
2024-09-11 14:21:56 -0400

    Added Reduce module

---
+++ src/Reduce.java
@@ -0,0 +1,16 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int step = 0;
+        int i = 100;
+        while (i != 0){
+            if (i % 2 == 0){
+                i = i / 2;
+            }
+            else {
+                i -= 1;
+            }
+            step++;
+        }
+        System.out.println(step);
+    }
+}
